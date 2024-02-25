4a283b1c84695b35487d5e901e6d77dd0ef8b3a4
mc3441272 <mc3441272@gmail.com>
2024-02-25 15:57:20 -0500

    Added state list

---
+++ 2024-crescendo/src/main/java/frc/robot/MainSubsystemsManager.java
@@ -0,0 +1,18 @@
+package frc.robot;
+
+public class MainSubsystemsManager {
+    private enum States {
+        NORMAL,
+        MANUAL_INTAKING,
+        AUTO_INTAKING,
+        AMP_PREPARED,
+        AMP_SCORING,
+        SUBWOOFER_PREPARED,
+        SUBWOOFER_SCORING,
+        PODIUM_PREPARED,
+        PODIUM_SCORING,
+        CLIMB_PREPARED,
+        CLIMB_EXTENSION_UP,
+        CLIMB_EXTENSION_DOWN
+    }
+}

b0801a91e3620f4dc53b61db45ae35f8f1560134
mc3441272 <mc3441272@gmail.com>
2024-03-02 11:20:59 -0500

    More work

--- 2024-crescendo/src/main/java/frc/robot/autonomous/autons/RedLeft2MidnotesAuto.java
+++ 2024-crescendo/src/main/java/frc/robot/autonomous/autons/RedLeft2MidnotesAuto.java
@@ -28,13 +28,14 @@ public class RedLeft2MidnotesAuto extends BaseAuto{
                 //         new IntakeCommand(intake, shooter),
                 //         new AutoCollectCommand(targeting, drive, shooter)
                 // ),
-                new DriveToPointCommand(drive, AutonomousPositions.MID_NOTE_5.getPose(), AutonomousPositions.SUBWOOFER_MIDDLE.translate(4,-4), config.setEndVelocity(3)),
-                new DriveToPointCommand(drive, AutonomousPositions.SUBWOOFER_MIDDLE.translate(4,-4), AutonomousPositions.SUBWOOFER_MIDDLE.translate(1.5,-3), config.setStartVelocity(3)),
-                new DriveToPointCommand(drive, AutonomousPositions.SUBWOOFER_MIDDLE.translate(1.5,-3), AutonomousPositions.SUBWOOFER_MIDDLE.translate(1,-1.5), config),
+                new DriveToPointCommand(drive, AutonomousPositions.MID_NOTE_5.getPose(), AutonomousPositions.SUBWOOFER_MIDDLE.translate(4,-4), config.setStartVelocity(0).setEndVelocity(3)),
+                new DriveToPointCommand(drive, AutonomousPositions.SUBWOOFER_MIDDLE.translate(4,-4), AutonomousPositions.SUBWOOFER_MIDDLE.translate(1.5,-3), config.setStartVelocity(3).setEndVelocity(3)),
+                new DriveToPointCommand(drive, AutonomousPositions.SUBWOOFER_MIDDLE.translate(1.5,-3), AutonomousPositions.SUBWOOFER_MIDDLE.translate(1,-1.5), config.setStartVelocity(3).setEndVelocity(0)),
                 // new AutoAimCommand(drive, speakerVision, APRILTAG_LIMELIGHT.LOCK_ERROR),
                 // new ShootCommand(shooter, elevator, 4500, 29.5),
                 // Add a midpoint
-                new DriveToPointCommand(drive, AutonomousPositions.SUBWOOFER_MIDDLE.translate(1,-1.5), AutonomousPositions.MID_NOTE_4.translate(-2,0), config.setStartVelocity(0)),
+                new DriveToPointCommand(drive, AutonomousPositions.SUBWOOFER_MIDDLE.translate(1,-1.5), AutonomousPositions.SUBWOOFER_MIDDLE.translate(-1,2), config.setStartVelocity(0).setEndVelocity(3)),
+                new DriveToPointCommand(drive, AutonomousPositions.SUBWOOFER_MIDDLE.translate(-1,2), AutonomousPositions.MID_NOTE_4.translate(-2,0), config.setStartVelocity(3).setEndVelocity(0)),
                 // new JointCommand(
                 //         new IntakeCommand(intake, shooter),
                 //         new AutoCollectCommand(targeting, drive, shooter)

c69af89deb435d8f0d7bcc7dcbceb1168bc15ce3
mc3441272 <mc3441272@gmail.com>
2024-03-02 16:12:53 -0500

    Still working on auto

--- 2024-crescendo/src/main/java/frc/robot/commands/DriveToPointCommand.java
+++ 2024-crescendo/src/main/java/frc/robot/commands/DriveToPointCommand.java
@@ -9,15 +9,12 @@ public class DriveToPointCommand extends Command {
     private Swerve drive;
     private SwerveTrajectory trajectory;
     private FollowerCommand followerCommand;
+    private Pose2d targetPose;
+    private TrajectoryConfig config;
     public DriveToPointCommand(Swerve drive, Pose2d targetPose, TrajectoryConfig config){
         this.drive = drive;
-        Pose2d currentPose = drive.getCurrentPos(); // Start by creating this so we don't call drive.getCurrentPos() a bunch of times
-        Pose2d modifiedStartPose = new Pose2d(currentPose.getTranslation(), 
-                Rotation2d.fromRadians(Math.atan2(targetPose.getY() - currentPose.getY(), targetPose.getX() - currentPose.getX()))); // The robot will go straight to the target point without any fancy curves
-        Pose2d modifiedEndPose = new Pose2d(targetPose.getTranslation(), 
-                Rotation2d.fromRadians(Math.atan2(targetPose.getY() - currentPose.getY(), targetPose.getX() - currentPose.getX())));
-        this.trajectory = AutonomousPositions.generate(config, modifiedStartPose, modifiedEndPose);
-        this.followerCommand = new FollowerCommand(drive, trajectory);
+        this.targetPose = targetPose;
+        this.config = config;
     }
     public DriveToPointCommand(Swerve drive, Pose2d startPose, Pose2d targetPose, TrajectoryConfig config){
         this.drive = drive;
@@ -30,6 +27,15 @@ public class DriveToPointCommand extends Command {
         this.followerCommand = new FollowerCommand(drive, trajectory);
     }
     public void initialize() {
+        if(followerCommand == null){
+            Pose2d currentPose = drive.getCurrentPos(); // Start by creating this so we don't call drive.getCurrentPos() a bunch of times
+            Pose2d modifiedStartPose = new Pose2d(currentPose.getTranslation(), 
+                    Rotation2d.fromRadians(Math.atan2(targetPose.getY() - currentPose.getY(), targetPose.getX() - currentPose.getX()))); // The robot will go straight to the target point without any fancy curves
+            Pose2d modifiedEndPose = new Pose2d(targetPose.getTranslation(), 
+                    Rotation2d.fromRadians(Math.atan2(targetPose.getY() - currentPose.getY(), targetPose.getX() - currentPose.getX())));
+            this.trajectory = AutonomousPositions.generate(config, modifiedStartPose, modifiedEndPose);
+            this.followerCommand = new FollowerCommand(drive, trajectory);
+        }
         this.followerCommand.initialize();
     }
     public boolean execute() {

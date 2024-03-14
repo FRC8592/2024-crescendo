60a77880f4ab599b945f9698f4c39a9f376fe23c
HishamAbb <HishamZabbasi@gmail.com>
2024-03-13 20:49:27 -0400

    Modified autoshoot command logic. Still needs work.

--- 2024-crescendo/src/main/java/frc/robot/commands/AutoShootCommand.java
+++ 2024-crescendo/src/main/java/frc/robot/commands/AutoShootCommand.java
@@ -21,6 +21,7 @@ public class AutoShootCommand extends Command {
     private Elevator elevator;
     private Shooter shooter;
     private Timer timer;
+    private boolean isShooting;
 
 
     public AutoShootCommand(Swerve drive, PoseVision vision, Elevator elevator, Shooter shooter) {
@@ -29,6 +30,7 @@ public class AutoShootCommand extends Command {
         this.elevator = elevator;
         this.shooter = shooter;
         this.timer = new Timer();
+        this.isShooting = false;
     }
     @Override
     public void initialize() {
@@ -40,23 +42,30 @@ public class AutoShootCommand extends Command {
         double omega = vision.visual_servo(0, 3, 4, 0); //TODO: make sure this works on both sides with the tag ID
         Logger.recordOutput("AutoShootCommand Omega", omega);
         double distance = vision.distanceToAprilTag(4);
-        /
         RangeEntry entry = RangeTable.get(distance);
         Logger.recordOutput("Distance to Tag 4", distance);
         elevator.setPivotAngleCustom(entry.pivotAngle);
-        shooter.setShootVelocity(entry.flywheelSpeed, entry.flywheelSpeed);
+        // shooter.setShootVelocity(0, 0);
         drive.drive(new ChassisSpeeds(0, 0, omega));
-        if (Math.abs(vision.getCurrTagX())<APRILTAG_LIMELIGHT.LOCK_ERROR && shooter.isReady() && elevator.isTargetAngle()){
+        if ((Math.abs(vision.getCurrTagX())<APRILTAG_LIMELIGHT.LOCK_ERROR && elevator.isTargetAngle()) || isShooting){
+            isShooting = true;
             SmartDashboard.putNumber("Ready To Shoot", distance);
             Logger.recordOutput("AutoShootCommand Shooting", true);
             this.timer.start();
-            if(this.timer.get() < 0.1){
+            if(this.timer.get() < 0.05){
                 shooter.setFeederVelocity(SHOOTER.OUTAKE_FEEDER_SPEED);
             }
             else if(this.timer.get() < SHOOTER.SHOOT_SCORE_TIME){
-                shooter.setFeederVelocity(SHOOTER.SHOOTING_FEEDER_SPEED);
+                shooter.setShootVelocity(entry.flywheelSpeed, entry.flywheelSpeed);
+                if(shooter.isReady()){
+                    shooter.setFeederVelocity(SHOOTER.SHOOTING_FEEDER_SPEED);
+                }
+                else{
+                    shooter.stopFeeders();
+                }
             }
             else{
+                shooter.stopFeeders();
                 return true; // end the command
             }
         }

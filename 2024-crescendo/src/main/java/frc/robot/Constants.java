1c4820ac9f0af34d22764aa343a6444646c6b059
Martin <mc3441272@gmail.com>
2024-04-12 19:29:27 -0400

    Top speed increase (for testing)

--- 2024-crescendo/src/main/java/frc/robot/Constants.java
+++ 2024-crescendo/src/main/java/frc/robot/Constants.java
@@ -335,7 +335,7 @@ public final class Constants {
 
 
         // public static final double MAX_VELOCITY_METERS_PER_SECOND = 4.95; // 4.5 + 0.1(4.5) to go from 5700 --> 6300
-        public static final double MAX_VELOCITY_METERS_PER_SECOND = 5.25; // crazy test
+        public static final double MAX_VELOCITY_METERS_PER_SECOND = 5.5; // crazy test
         public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND /
                 Math.hypot(SWERVE.DRIVE_TRAIN_WIDTH / 2.0, DRIVE_TRAIN_LENGTH / 2.0);
 

c76841fd0e5eaba0b5be770a74d6bffbb3036a7d
Martin <mc3441272@gmail.com>
2024-05-06 16:47:26 -0400

    RobotContainer compiles

--- 2024-cmd/src/main/java/frc/robot/RobotContainer.java
+++ 2024-cmd/src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,7 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
+import frc.robot.Constants.*;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.Swerve;
@@ -27,7 +27,7 @@ public class RobotContainer {
 
     // Replace with CommandPS4Controller or CommandJoystick if needed
     private final CommandXboxController m_driverController = new CommandXboxController(
-            OperatorConstants.kDriverControllerPort);
+            CONTROLLERS.DRIVER_PORT);
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.

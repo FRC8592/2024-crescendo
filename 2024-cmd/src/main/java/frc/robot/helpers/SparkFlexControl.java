08e31673e1f092ec8a1a050ff730ab26a9a307c9
Martin <mc3441272@gmail.com>
2024-05-09 13:10:54 -0400

    Shooter prime & shoot commands (no intake yet), SparkFlexControl

---
+++ 2024-cmd/src/main/java/frc/robot/helpers/SparkFlexControl.java
@@ -0,0 +1,120 @@
+package frc.robot.helpers;
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.REVLibError;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkPIDController.AccelStrategy;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkBase.SoftLimitDirection;
+
+import com.revrobotics.CANSparkLowLevel.PeriodicFrame;
+
+public class SparkFlexControl {
+    public CANSparkFlex motor;
+    public RelativeEncoder motorEncoder;
+    public SparkPIDController motorControl;
+
+    /**
+     * Create a Newton2 controller for a NEO Vortex (with Spark Flex)
+     * @param motorCanID
+     * @param coastMode If true, the motor coasts when asked to apply 0 power. Otherwise, it brakes.
+     */
+    public SparkFlexControl(int motorCanID, boolean coastMode){
+        motor = new CANSparkFlex(motorCanID, MotorType.kBrushless);
+        motor.restoreFactoryDefaults();
+
+        //Set update status to 20 Hz
+        motor.setPeriodicFramePeriod(PeriodicFrame.kStatus0, 50);
+        
+        motorControl = motor.getPIDController();
+        motorEncoder = motor.getEncoder();
+        if (coastMode){
+            motor.setIdleMode(IdleMode.kCoast);
+        } else{
+            motor.setIdleMode(IdleMode.kBrake);
+        }
+        motor.set(0);
+    }
+
+    public void setVelocity(double RPM){
+        motorControl.setReference(RPM, com.revrobotics.CANSparkBase.ControlType.kVelocity); 
+    }
+
+    public void setVelocity(double RPM, int slotID){
+        motorControl.setReference(RPM, com.revrobotics.CANSparkBase.ControlType.kVelocity, slotID); 
+    }
+
+    public void setPosition(double rotations){
+        motorControl.setReference(rotations, com.revrobotics.CANSparkBase.ControlType.kPosition); 
+    }
+
+    public void setPercentOutput(double power){
+        motor.set(power);
+    }
+
+    public void stop(){
+        motor.set(0);
+    }
+
+    public void stopSmartVelocity(){
+        motorControl.setReference(0, ControlType.kSmartVelocity, 0);
+    }
+
+    public void setPIDF(double P, double I, double D, double FF, int slotID){
+        motorControl =  motor.getPIDController();
+        motorControl.setP(P,slotID);
+        motorControl.setI(I, slotID);
+        motorControl.setD(D, slotID);
+        motorControl.setFF(FF, slotID);
+        motorControl.setSmartMotionAccelStrategy(AccelStrategy.kTrapezoidal,slotID);
+        motorControl.setSmartMotionAllowedClosedLoopError(0.02, slotID); // NOTE: THIS TOLERNACE IS IMPORTANT!!!!
+    }
+
+    public double getVelocity(){
+        return motorEncoder.getVelocity();
+    }
+    
+    public double getPosition(){
+        return motorEncoder.getPosition();
+    }
+
+    public void setInverted(){
+        motor.setInverted(true);
+    }
+
+    public double getTicks(){
+        return motorEncoder.getPosition()*4096;
+    }
+
+
+    public void setFollower(SparkFlexControl motorToFollow){
+        motor.follow(motorToFollow.motor);
+    }
+
+    public void setSoftLimit(SoftLimitDirection direction, double rotations){
+        motor.setSoftLimit(direction, (float)rotations);
+    }
+
+    public void setPositionSmartMotion(double rotations){
+        motorControl.setReference(rotations, ControlType.kSmartMotion, 0);
+        SmartDashboard.putNumber("smart motion", rotations);
+    }
+
+    public void setMaxVelocity(double maxVelocity, int slotID){
+        motorControl.setSmartMotionMaxVelocity(maxVelocity, slotID);
+    }
+    public void setMaxAcceleration(double maxAccel, int slotID){
+        motorControl.setSmartMotionMaxAccel(maxAccel, slotID);
+    }
+    public void follow(SparkFlexControl sfc, boolean inverted) {
+        this.motor.follow(sfc.motor, inverted);
+    }
+    public void setCurrentLimit(int stallLimit, int fullRPMLimit){
+        this.motor.setSmartCurrentLimit(stallLimit, fullRPMLimit);
+    }
+}
--- 2024-cmd/src/main/java/frc/robot/subsystems/Shooter.java
+++ 2024-cmd/src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,44 +6,170 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Constants.*;
+
+import java.util.function.IntSupplier;
+
+import org.littletonrobotics.junction.Logger;
+import frc.robot.helpers.*;
 
 public class Shooter extends SubsystemBase {
-    /** Creates a new ExampleSubsystem. */
+    private SparkFlexControl leftShooterMotor;
+    private SparkFlexControl rightShooterMotor;
+    private SparkFlexControl feederMotor;
+
+    private DigitalInput bottomBeamBreak;
+    private DigitalInput topBeamBreak;
+    private DigitalInput middleBeamBreak;
+
+    int leftTargetSpeed = 0;
+    int rightTargetSpeed = 0;
+
     public Shooter() {
+        leftShooterMotor = new SparkFlexControl (CAN.TOP_SHOOTER_MOTOR_CAN_ID, false);
+
+        leftShooterMotor.setPIDF (SHOOTER.LEFT_SHOOTER_MOTOR_kP,  SHOOTER.LEFT_SHOOTER_MOTOR_kI,  SHOOTER.LEFT_SHOOTER_MOTOR_kD,  SHOOTER.LEFT_SHOOTER_MOTOR_kF,  0);
+        leftShooterMotor.motorControl.setIZone (SHOOTER.SHOOTER_MOTOR_IZONE);
+
+        leftShooterMotor.setCurrentLimit (POWER.LEFT_SHOOTER_MOTOR_CURRENT_LIMIT,  POWER.LEFT_SHOOTER_MOTOR_CURRENT_LIMIT);
+        leftShooterMotor.setInverted();
+
+        leftShooterMotor.setPercentOutput(0);
+
+
+        rightShooterMotor = new SparkFlexControl(CAN.BOTTOM_SHOOTER_MOTOR_CAN_ID, false);
+
+        rightShooterMotor.setPIDF(SHOOTER.RIGHT_SHOOTER_MOTOR_kP, SHOOTER.RIGHT_SHOOTER_MOTOR_kI, SHOOTER.RIGHT_SHOOTER_MOTOR_kD, SHOOTER.RIGHT_SHOOTER_MOTOR_kF, 0);
+
+        rightShooterMotor.setCurrentLimit(POWER.RIGHT_SHOOTER_MOTOR_CURRENT_LIMIT, POWER.RIGHT_SHOOTER_MOTOR_CURRENT_LIMIT);
+        rightShooterMotor.setPercentOutput(0);
+
+        rightShooterMotor.motorControl.setIZone(SHOOTER.SHOOTER_MOTOR_IZONE);
+
+
+        feederMotor = new SparkFlexControl(CAN.FEEDER_MOTOR_CAN_ID, false);
+
+        feederMotor.setPIDF(SHOOTER.FEEDER_MOTOR_LOADED_kP, SHOOTER.FEEDER_MOTOR_LOADED_kI, SHOOTER.FEEDER_MOTOR_LOADED_kD, SHOOTER.FEEDER_MOTOR_LOADED_kF, 1);
+        feederMotor.setPIDF(SHOOTER.FEEDER_MOTOR_OUTAKE_kP, SHOOTER.FEEDER_MOTOR_OUTAKE_kI, SHOOTER.FEEDER_MOTOR_OUTAKE_kD, SHOOTER.FEEDER_MOTOR_OUTAKE_kF, 2);
+        feederMotor.setPIDF(SHOOTER.FEEDER_MOTOR_kP,SHOOTER.FEEDER_MOTOR_kI,SHOOTER.FEEDER_MOTOR_kD,SHOOTER.FEEDER_MOTOR_kF,0);
+        feederMotor.motorControl.setIZone(SHOOTER.FEEDER_MOTOR_LOADED_IZONE, 1);
+        feederMotor.motorControl.setIZone(SHOOTER.FEEDER_MOTOR_OUTAKE_IZONE, 2);
+
+        feederMotor.setCurrentLimit(POWER.FEEDER_MOTOR_CURRENT_LIMIT,POWER.FEEDER_MOTOR_CURRENT_LIMIT);
+        feederMotor.setInverted();
+
+        feederMotor.setPercentOutput(0);
+
+
+        bottomBeamBreak = new DigitalInput(SHOOTER.BOTTOM_BEAM_BREAK_DIO_PORT);
+        topBeamBreak = new DigitalInput   (SHOOTER.TOP_BEAM_BREAK_DIO_PORT);
+        middleBeamBreak = new DigitalInput(SHOOTER.MIDDLE_BEAM_BREAK_DIO_PORT);
+    }
+
+    // This command can't easily be made using the convenient methods from SubsystemBase,
+    // so we create a private class and return an instance of that (scroll down) instead.
+    private class ShooterPrimeCommand extends Command{
+        private IntSupplier leftRPM;
+        private IntSupplier rightRPM;
+        public ShooterPrimeCommand(int leftRPM, int rightRPM){
+            this.leftRPM = () -> {return leftRPM;};
+            this.rightRPM = () -> {return rightRPM;};
+            addRequirements(Shooter.this); //Refers to the instance of Shooter that parents this instance of ShooterPrimeCommand.
+        }
+        public ShooterPrimeCommand(IntSupplier leftRPM, IntSupplier rightRPM){
+            this.leftRPM = leftRPM;
+            this.rightRPM = rightRPM;
+            addRequirements(Shooter.this); //Refers to the instance of Shooter that parents this instance of ShooterPrimeCommand.
+        }
+        public void initialize(){}
+        public void execute(){
+            leftTargetSpeed = leftRPM.getAsInt();
+            rightTargetSpeed = rightRPM.getAsInt();
+            leftShooterMotor.setVelocity(leftTargetSpeed);
+            rightShooterMotor.setVelocity(rightTargetSpeed);
+        }
+        public void end(boolean interrupted){
+            if(interrupted){
+                leftTargetSpeed = 0;
+                rightTargetSpeed = 0;
+                leftShooterMotor.setVelocity(leftTargetSpeed);
+                rightShooterMotor.setVelocity(rightTargetSpeed);
+            }
+        }
+        public boolean isFinished(){return readyToShoot();}
+    }
+    private class FireCommand extends Command{ // Named FireCommand instead of ShootCommand for clarity that this DOESN'T prime
+        private Timer timer;
+        public FireCommand(){
+            this.timer = new Timer();
+            addRequirements(Shooter.this);
+        }
+        public void initialize(){this.timer.start();}
+        public void execute(){feederMotor.setPercentOutput(SHOOTER.SHOOTING_FEEDER_POWER);}
+        public void end(boolean interrupted){}
+        public boolean isFinished(){return timer.hasElapsed(SHOOTER.SHOOT_SCORE_TIME);}
+    }
+    public Command shooterPrimeCommand(int leftRPM, int rightRPM){
+        return new ShooterPrimeCommand(leftRPM, rightRPM).withInterruptBehavior(InterruptionBehavior.kCancelIncoming);
+    }
+    public Command fireCommand(){
+        return new FireCommand().withInterruptBehavior(InterruptionBehavior.kCancelIncoming);
+    }
+    public Command stopCommand(){
+        return runOnce(() -> {
+            leftShooterMotor.setVelocity(0);
+            leftTargetSpeed = 0;
+            rightShooterMotor.setVelocity(0);
+            rightTargetSpeed = 0;
+            feederMotor.setVelocity(0);
+        });
+    }
+
+    public void periodic() {
+        Logger.recordOutput(SHOOTER.LOG_PATH+"MotorRPMs/LeftTargetSpeed", leftTargetSpeed);
+        Logger.recordOutput(SHOOTER.LOG_PATH+"MotorRPMs/RightTargetSpeed", rightTargetSpeed);
+        Logger.recordOutput(SHOOTER.LOG_PATH+"MotorRPMs/RightShooterSpeed", rightShooterMotor.getVelocity());
+        Logger.recordOutput(SHOOTER.LOG_PATH+"MotorRPMs/LeftShooterSpeed", leftShooterMotor.getVelocity());
+        Logger.recordOutput(SHOOTER.LOG_PATH+"MotorRPMs/FeederSpeed", feederMotor.getVelocity());
+        Logger.recordOutput(SHOOTER.LOG_PATH+"ReadyToShoot", readyToShoot());
+        Logger.recordOutput(SHOOTER.LOG_PATH+"BeamBreaks/Bottom", bottomBeamBreak.get());
+        Logger.recordOutput(SHOOTER.LOG_PATH+"BeamBreaks/Middle", middleBeamBreak.get());
+        Logger.recordOutput(SHOOTER.LOG_PATH+"BeamBreaks/Top", topBeamBreak.get());
+    }
+
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
     }
 
     /**
-     * Example command factory method.
-     *
-     * @return a command
+     * checks if flywheels are at target speed to shoot
+     * and resets hasNote cuz that means we're gonna shoot
+     * 
+     * @apiNote You must call this method before shooting
      */
-    public Command exampleMethodCommand() {
-        // Inline construction of command goes here.
-        // Subsystem::RunOnce implicitly requires `this` subsystem.
-        return runOnce(
-                () -> {
-                    /* one-time action goes here */
-                });
-    }
-
-    /**
-     * An example method querying a boolean state of the subsystem (for example, a
-     * digital sensor).
-     *
-     * @return value of some boolean subsystem state, such as a digital sensor.
-     */
-    public boolean exampleCondition() {
-        // Query some boolean state, such as a digital sensor.
+    private boolean readyToShoot() {
+        if (Math.abs(leftShooterMotor.getVelocity() - leftTargetSpeed) < SHOOTER.FHYWHEEL_SPEED_ACCEPTABLE_RANGE
+                // && Math.abs(rightShooterMotor.getVelocity() - rightTargetSpeed) < SHOOTER.FHYWHEEL_SPEED_ACCEPTABLE_RANGE
+                && leftTargetSpeed > 1000
+                && rightTargetSpeed > 1000) {
+            return true;
+        }
         return false;
     }
 
-    @Override
-    public void periodic() {
-        // This method will be called once per scheduler run
+    private boolean isBottomBeamBreakTripped(){
+        return !bottomBeamBreak.get(); //The beam break pulls low when triggered (notice exclamation point)
     }
 
-    @Override
-    public void simulationPeriodic() {
-        // This method will be called once per scheduler run during simulation
+    private boolean isTopBeamBreakTripped(){
+        return !topBeamBreak.get(); //The beam break pulls low when triggered (notice exclamation point)
+    }
+
+    private boolean isMiddleBeamBreakTripped(){
+        return !middleBeamBreak.get(); //The beam break pulls low when triggered (notice exclamation point)
     }
 }

24599fb1621415efc0ffd866c79649fc32d24328
Martin <mc3441272@gmail.com>
2024-05-09 13:11:26 -0400

    Intake subsystem theoretically done

--- 2024-cmd/src/main/java/frc/robot/subsystems/Intake.java
+++ 2024-cmd/src/main/java/frc/robot/subsystems/Intake.java
@@ -4,45 +4,56 @@
 
 package frc.robot.subsystems;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.helpers.*;
+import frc.robot.Constants.*;
 
 public class Intake extends SubsystemBase {
-    /** Creates a new ExampleSubsystem. */
+    private SparkFlexControl intakeMotor;
+    private double targetIntakeVelocity = 0;
     public Intake() {
+        intakeMotor = new SparkFlexControl(CAN.INTAKE_MOTOR_CAN_ID, true);
+        intakeMotor.setPIDF(INTAKE.MOTOR_kP, INTAKE.MOTOR_kI, INTAKE.MOTOR_kD, INTAKE.MOTOR_kFF, 0);
+        intakeMotor.setCurrentLimit(POWER.INTAKE_MOTOR_CURRENT_LIMIT, POWER.INTAKE_MOTOR_CURRENT_LIMIT);
     }
 
-    /**
-     * Example command factory method.
-     *
-     * @return a command
-     */
-    public Command exampleMethodCommand() {
-        // Inline construction of command goes here.
-        // Subsystem::RunOnce implicitly requires `this` subsystem.
-        return runOnce(
-                () -> {
-                    /* one-time action goes here */
-                });
+    public Command outakeCommand() {
+        return runEnd(() -> { 
+            // Run continuously until interrupted
+            targetIntakeVelocity = INTAKE.OUTAKE_VELOCITY;
+            intakeMotor.setVelocity(INTAKE.OUTAKE_VELOCITY);
+        }, () -> { 
+            // Run once when interrupted
+            targetIntakeVelocity = 0;
+            intakeMotor.setVelocity(0);
+        });
     }
 
-    /**
-     * An example method querying a boolean state of the subsystem (for example, a
-     * digital sensor).
-     *
-     * @return value of some boolean subsystem state, such as a digital sensor.
-     */
-    public boolean exampleCondition() {
-        // Query some boolean state, such as a digital sensor.
-        return false;
+    public Command intakeCommand(){
+        return runEnd(() -> {
+            targetIntakeVelocity = INTAKE.INTAKE_VELOCITY;
+            intakeMotor.setVelocity(INTAKE.INTAKE_VELOCITY);
+        }, () -> {
+            targetIntakeVelocity = 0;
+            intakeMotor.setVelocity(0);
+        });
     }
 
-    @Override
+    public Command stopCommand(){
+        return runOnce(() -> {
+            targetIntakeVelocity = 0;
+            intakeMotor.setVelocity(0);
+        });
+    }
+
     public void periodic() {
-        // This method will be called once per scheduler run
+        Logger.recordOutput(INTAKE.LOG_PATH+"IntakeVelocityRPM", intakeMotor.getVelocity());
+        Logger.recordOutput(INTAKE.LOG_PATH+"IntakeTargetRPM", targetIntakeVelocity);
     }
 
-    @Override
     public void simulationPeriodic() {
         // This method will be called once per scheduler run during simulation
     }

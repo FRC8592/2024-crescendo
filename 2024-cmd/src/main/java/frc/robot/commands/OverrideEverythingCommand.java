c4b747b81a6462ef76889519155cd4c0b1bcbf93
Martin <mc3441272@gmail.com>
2024-05-09 13:12:12 -0400

    OverrideEverythingCommand (clears queue, then runs user command) in preparation for StowCommand

---
+++ 2024-cmd/src/main/java/frc/robot/commands/OverrideEverythingCommand.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.ProxyCommand;
+
+public class OverrideEverythingCommand extends Command {
+    Command command;
+    public OverrideEverythingCommand(Command command) {
+        this.command = command;
+    }
+
+    public void initialize() {
+    }
+
+    public void execute() {
+        CommandScheduler.getInstance().cancelAll();
+    }
+
+    public void end(boolean interrupted) {
+        CommandScheduler.getInstance().schedule(command);
+    }
+
+    public boolean isFinished() {
+        return false;
+    }
+}
